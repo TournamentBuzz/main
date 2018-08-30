6c4210b7fa102b48b2b33f4afc2f6012a9841042
Maxwell Bronstein <mbronstein3@gatech.edu>
2018-08-30 16:59:32 -0400

    Using enzyme to perform proper unit tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "./App";
+import HomePage from "./views/HomePage";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders <HomePage /> without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(HomePage)).toHaveLength(1);
 });
