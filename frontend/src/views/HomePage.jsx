9818d412cbf0de1f9ef72d66509c900fda767ccb
alex-stetson <astetson@gatech.edu>
2018-09-03 21:37:43 -0400

    Added new header links layout

--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -8,6 +8,7 @@ import Header from "components/Header/Header.jsx";
 import headerLinksStyle from "assets/jss/components/headerLinksStyle.jsx";
 
 import NoAuthHeaderLinks from "components/Header/NoAuthHeaderLinks.jsx";
+//import AuthHeaderLinks from "components/Header/AuthHeaderLinks.jsx";
 
 class HomePage extends React.Component {
   render() {
