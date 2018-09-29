c42afc7b78e53d812377173be1d6625ff9015960
Dennis Li <23002167+dli357@users.noreply.github.com>
2018-09-28 20:10:06 -0400

    Added get specific tournament endpoint

--- backend/controllers/tournaments/id.js
+++ backend/controllers/tournaments/id.js
@@ -2,14 +2,11 @@
 
 const express = require("express");
 const router = express.Router();
-const sqlwrapper = require("../model/wrapper");
-const connection = require("../model/connect");
 
-const id = require("./tournaments/id");
+const sqlwrapper = require("../../model/wrapper");
+const connection = require("../../model/connect");
 
-router.use("/id", id);
-
-router.get("/", async function(req, res, next) {
+router.get("/:id", async function(req, res, next) {
   try {
     const c = connection.connect(
       req.app.get("databaseConfig").host,
@@ -17,9 +14,10 @@ router.get("/", async function(req, res, next) {
       req.app.get("databaseConfig").password,
       req.app.get("databaseConfig").schema
     );
-    const results = await sqlwrapper.getTournaments(c);
+    const tournamentObject = await sqlwrapper.getTournament(c, req.param("id"));
+    console.log(tournamentObject);
     res.status(200);
-    res.json({ tournaments: results });
+    res.json({ tournament: tournamentObject });
   } catch (err) {
     next(err);
   }
