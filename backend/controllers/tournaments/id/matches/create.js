b95fe13c44de172f7c043853d9e20b76b68cf545
Dennis Li <23002167+dli357@users.noreply.github.com>
2018-10-18 17:28:53 -0400

    Added manual match functionality - create, edit, delete

---
+++ backend/controllers/tournaments/id/matches/create.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const express = require("express");
+const sqlwrapper = require("../../model/wrapper");
+const connection = require("../../model/connect");
+const router = express.Router();
+
+router.post("", async (req, res, next) => {
+  if (!req.body) {
+    const err = new Error("Malformed Request");
+    err.status = 400;
+    next(err);
+    return;
+  }
+  if (req.headers.id !== null) {
+    try {
+      const c = connection.connect(
+        req.app.get("databaseConfig").host,
+        req.app.get("databaseConfig").username,
+        req.app.get("databaseConfig").password,
+        req.app.get("databaseConfig").schema
+      );
+      const tournamentObject = await sqlwrapper.getTournament(
+        c,
+        req.routeParams.id
+      );
+      if (!tournamentObject[0]) {
+        const err = new Error("Tournament does not exist!");
+        err.status = 404;
+        next(err);
+        return;
+      }
+      if (req.headers.id === tournamentObject[0].creator) {
+        const rows = await sqlwrapper.createMatch(
+          c,
+          req.body.location,
+          req.body.score,
+          req.body.matchTime,
+          req.body.matchName,
+          req.routeParams.id,
+          req.body.teamA,
+          req.body.teamB
+        );
+        res.status(200);
+        res.json({ tournamentId: req.routeParams.id, matchId: rows.insertId });
+      } else {
+        const err = new Error("You cannot create matches for this tournament!");
+        err.status = 401;
+        next(err);
+      }
+    } catch (err) {
+      err.status = 500;
+      next(err);
+    }
+  } else {
+    const err = new Error("Invalid Credentials");
+    err.status = 401;
+    next(err);
+  }
+});
+
+module.exports = router;
---
+++ backend/controllers/tournaments/id/matches/edit.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const express = require("express");
+const sqlwrapper = require("../../model/wrapper");
+const connection = require("../../model/connect");
+const router = express.Router();
+
+router.post("", async (req, res, next) => {
+  if (!req.body || req.body.matchId < 0) {
+    const err = new Error("Malformed Request");
+    err.status = 400;
+    next(err);
+    return;
+  }
+  try {
+    const c = connection.connect(
+      req.app.get("databaseConfig").host,
+      req.app.get("databaseConfig").username,
+      req.app.get("databaseConfig").password,
+      req.app.get("databaseConfig").schema
+    );
+    const matchObject = await sqlwrapper.getMatch(c, req.body.matchId);
+    if (!matchObject[0]) {
+      const err = new Error("Match does not exist!");
+      err.status = 404;
+      next(err);
+      return;
+    }
+    const tournamentObject = await sqlwrapper.getTournament(
+      c,
+      matchObject[0].tournament
+    );
+    if (!tournamentObject[0]) {
+      const err = new Error(
+        "Tournament does not exist, should not have happened!"
+      );
+      next(err);
+      return;
+    }
+    if (req.headers.id === tournamentObject[0].creator) {
+      await sqlwrapper.updateMatch(
+        c,
+        req.body.matchId,
+        req.body.location,
+        req.body.score,
+        req.body.matchTime,
+        req.body.matchName,
+        req.routeParams.id,
+        req.body.teamA,
+        req.body.teamB
+      );
+      res.status(200);
+      res.json({
+        tournamentId: tournamentObject.id,
+        matchId: req.body.matchId
+      });
+    } else {
+      const err = new Error("You cannot edit this match!");
+      err.status = 401;
+      next(err);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
