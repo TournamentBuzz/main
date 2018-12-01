f1351c75b83ce201d71e4d0672120e82dfa52bed
Dennis Li <23002167+dli357@users.noreply.github.com>
2018-11-30 23:47:48 -0500

    Pass in the right variable so that powers of two numbers of teams generates properly for single elim

--- backend/controllers/tournaments/id/generate.js
+++ backend/controllers/tournaments/id/generate.js
@@ -58,7 +58,7 @@ async function generateSingleElimBracket(c, teams, tournamentId) {
   }
   let currentMatchups = await generateMatchLayerFromTeams(
     c,
-    teams,
+    teamsCopy,
     tournamentId
   );
   while (currentMatchups.length !== 1) {
