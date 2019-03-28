09491c913911395fe472ba5076a5de3d94dc1033
Dennis Li <23002167+dli357@users.noreply.github.com>
2019-03-28 17:23:03 -0400

    Fixed path bug

--- backend/controllers/user/tournaments.js
+++ backend/controllers/user/tournaments.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const sqlwrapper = require("../model/wrapper");
+const sqlwrapper = require("../../model/wrapper");
 
 router.get("/", async function(req, res, next) {
   try {
